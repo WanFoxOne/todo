34f3cd193938fa3748bfa376ac08943a040ffe8b
WanFoxOne <account@corler.pro>
2018-09-18 18:15:28 +0200

    Create task component

---
+++ components/ui/Task.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="card">
+        <div class="card-content">
+            <p
+                class="title"
+                :class="{completed}">{{ title }}</p>
+            <p class="subtitle">
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </p>
+        </div>
+        <footer class="card-footer">
+            <a
+                href="#"
+                class="card-footer-item">Éditer</a>
+            <a
+                href="#"
+                class="card-footer-item">Valider</a>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Task',
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            completed: {
+                type: Boolean,
+                default: false
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+    div.card {
+
+        p.title {
+
+            &.completed {
+                color: #b0b0b0;
+                text-decoration: line-through;
+            }
+        }
+    }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
     <section class="container">
 
         <div class="timeline">
-
             <div class="timeline-item">
                 <div class="timeline-marker is-primary"></div>
                 <div class="timeline-content">
@@ -14,80 +13,12 @@
 
         <div class="columns is-multiline is-mobile">
             <div class="column is-full-mobile is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
-                <div class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">
-                            Faire les courses
-                        </p>
-                        <a
-                            href="#"
-                            class="card-header-icon"
-                            aria-label="more options">
-                            <span class="icon">
-                                <i
-                                    class="fas fa-angle-down"
-                                    aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </header>
-                    <div class="card-content">
-                        <div class="content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-                            <br>
-                            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                        </div>
-                    </div>
-                    <footer class="card-footer">
-                        <a
-                            href="#"
-                            class="card-footer-item">Save</a>
-                        <a
-                            href="#"
-                            class="card-footer-item">Edit</a>
-                        <a
-                            href="#"
-                            class="card-footer-item">Delete</a>
-                    </footer>
-                </div>
+                <task 
+                    title="Une tâche" 
+                    completed="true"/>
             </div>
             <div class="column is-full-mobile is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
-                <div class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">
-                            Component
-                        </p>
-                        <a
-                            href="#"
-                            class="card-header-icon"
-                            aria-label="more options">
-                            <span class="icon">
-                                <i
-                                    class="fas fa-angle-down"
-                                    aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </header>
-                    <div class="card-content">
-                        <div class="content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-                            <br>
-                            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                        </div>
-                    </div>
-                    <footer class="card-footer">
-                        <a
-                            href="#"
-                            class="card-footer-item">Save</a>
-                        <a
-                            href="#"
-                            class="card-footer-item">Edit</a>
-                        <a
-                            href="#"
-                            class="card-footer-item">Delete</a>
-                    </footer>
-                </div>
+                <task title="Une autre tâche"/>
             </div>
         </div>
 
@@ -95,7 +26,12 @@
 </template>
 
 <script>
+    import Task from '~/components/ui/Task';
+
     export default {
+        components: {
+            Task
+        },
         fetch( { store, params } ) {
 
             // Set banner content
@@ -107,8 +43,13 @@
 </script>
 
 <style lang="scss" scoped>
-    section {
 
+    @media all and (max-width: 1087px) {
+
+        section {
+            margin: 0 25px;
+        }
     }
+
 </style>
 
